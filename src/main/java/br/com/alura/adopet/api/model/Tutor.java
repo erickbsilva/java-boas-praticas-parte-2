8bdeb3f69b78d48362433115c0625c241c887a54
erick <erickbarretodasilva@gmail.com>
2025-10-07 19:01:31 -0300

    melhorando o código

--- src/main/java/br/com/alura/adopet/api/model/Tutor.java
+++ src/main/java/br/com/alura/adopet/api/model/Tutor.java
@@ -32,7 +32,7 @@ public class Tutor {
     @Column(name = "email")
     private String email;
 
-    @OneToMany(mappedBy = "tutor", fetch = FetchType.EAGER)
+    @OneToMany(mappedBy = "tutor") // quando one to many, LAZY CARREGA POR PADRÃO
     @JsonManagedReference("tutor_adocoes")
     private List<Adocao> adocoes;
 
